b4d520c05f7ce47000ea3e4efca555dd43fe99bc
minhnghialk <nghia13031995@gmail.com>
2023-10-17 10:22:44 +0700

    add

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,22 @@ import * as multer from 'multer';
 
 //upload file
 import { json, urlencoded } from 'express';
-
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
-  dotenv.config(); 
-  const app = await NestFactory.create(AppModule,{cors:true});
+  dotenv.config();
+  const app = await NestFactory.create(AppModule, { cors: true });
 
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
   //upload file
   const upload = multer({ dest: 'uploads/' });
   app.use(upload.any());
 
-
-
-
-
-  
   await app.listen(3000);
 }
 bootstrap();
