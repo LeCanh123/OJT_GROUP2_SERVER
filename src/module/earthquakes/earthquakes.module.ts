a07326e2e687490546ca180c8e7bf4835996663e
LeCanh123 <ngoccanh124937@gmail.com>
2023-10-23 11:32:16 +0700

    fix category

--- src/module/earthquakes/earthquakes.module.ts
+++ src/module/earthquakes/earthquakes.module.ts
@@ -3,10 +3,14 @@ import { EarthquakesService } from './earthquakes.service';
 import { EarthquakesController } from './earthquakes.controller';
 import { earthquakeProviders } from './earthquake.providers';
 import { DatabaseModule } from '../typeorms/database.module';
+import { CategorysService } from '../categorys/categorys.service';
+import { categorysProviders } from '../categorys/categorys.providers';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [EarthquakesController],
-  providers: [EarthquakesService,...earthquakeProviders,],
+  providers: [EarthquakesService,...earthquakeProviders,
+    CategorysService, ...categorysProviders,
+  ],
 })
 export class EarthquakesModule {}
