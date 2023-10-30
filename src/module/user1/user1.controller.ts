7cc36d3da8e272673c62acf0736e4974621e2104
minhnghialk <nghia13031995@gmail.com>
2023-10-30 12:00:21 +0700

    render_user

--- src/module/user1/user1.controller.ts
+++ src/module/user1/user1.controller.ts
@@ -7,29 +7,30 @@ import {
   Param,
   Delete,
   Res,
+  Req,
 } from '@nestjs/common';
-import { UsersService1 } from './user1.service'; 
+import { UsersService1 } from './user1.service';
 import { Response } from 'express';
 
 @Controller('users1')
 export class UsersController1 {
   constructor(private readonly usersService: UsersService1) {}
 
-  @Post("/facebooklogin")
+  @Post('/facebooklogin')
   facebooklogin(@Body() data) {
     return this.usersService.facebooklogin(data);
   }
 
-  @Post("/googlelogin")
+  @Post('/googlelogin')
   googlelogin(@Body() data) {
-    return this.usersService.googlelogin(data)
+    return this.usersService.googlelogin(data);
   }
 
-  @Post("/checktoken")
-  async checktoken(@Body() data,@Res() res: Response) {
+  @Post('/checktoken')
+  async checktoken(@Body() data, @Res() res: Response) {
     try {
       const { token } = data;
-      const result = await this.usersService.userChecktoken(token)
+      const result = await this.usersService.userChecktoken(token);
       if (result.status) {
         return res.status(200).json(result);
       }
@@ -38,7 +39,24 @@ export class UsersController1 {
       return res.status(201).json({
         status: false,
         message: 'Lỗi hệ thống',
-        err:error
+        err: error,
+      });
+    }
+  }
+
+  @Get('/getusers')
+  async getAllUser(@Req() req: Request, @Res() res: Response) {
+    try {
+      const result = await this.usersService.getAllUser();
+      if (result.status) {
+        return res.status(200).json(result);
+      }
+      return res.status(201).json(result);
+    } catch (error) {
+      return res.status(201).json({
+        status: false,
+        message: 'Lỗi hệ thống',
+        err: error,
       });
     }
   }
